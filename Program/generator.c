fb753a63dfd39dd7dd1e629ffaaa48e3b312117c
Leighton Lilford <leighton.lilford@bhpilliton.com>
2017-01-08 20:41:51 +0800

    basic fifo

--- Program/generator.c
+++ Program/generator.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
 
-char* generate(){
+#define INGOING "traffic.fifo"
+#define BUFFER 200
+
+int generate(){
+    printf("Generating\n");
     ///Generate random traffic to fifo file
+    mkfifo(INGOING, 0666);
+    printf("Made fifo\n");
+    int out_fd=open(INGOING, O_WRONLY);
+    printf("opened fifo for writing\n");
+    if (out_fd==-1) {
+        perror("open error");
+    }
+    pid_t pid = fork();
+    if (pid == 0){
+        // child process
+        while (1) {
+            printf("Writing to fifo\n");
+            char* input = "Hello World";
+            if (write(out_fd, input, strlen(input))==-1) {
+                perror("write error");
+            }
+        }
+    }
+    else if (pid > 0){
+        // parent
+        return 0;
+    }
+    else{
+        // fork failed
+        printf("fork() failed!\n");
+        return 1;
+    }
     
 }
--- Program/main.c
+++ Program/main.c
@@ -1,7 +1,22 @@
 #include <printf.h>
 #include "generator.c"
 
+#define BUFFER 200
+
 int main(int argc, char *argv[]) {
-    printf("Starting");
+    printf("Starting\n");
+
+    generate();
+    char in[BUFFER];
+
+    int in_fd=open(INGOING, O_RDONLY);
+    if (in_fd==-1) {
+        perror("open error");
+        exit(-1);
+    }
+    while (read(in_fd, in, BUFFER)>0) {
+        printf("Received data %s\n", in);
+    }
+
     return 0;
 }
