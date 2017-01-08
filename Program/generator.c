e3ee2a04ea2d0e26614a6d8b76fa893a199d32a4
Leighton Lilford <leighton.lilford@bhpilliton.com>
2017-01-08 20:20:54 +0800

    Init

---
+++ Program/generator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+char* generate(){
+    ///Generate random traffic to fifo file
+    
+}
---
+++ Program/main.c
@@ -0,0 +1,7 @@
+#include <printf.h>
+#include "generator.c"
+
+int main(int argc, char *argv[]) {
+    printf("Starting");
+    return 0;
+}
