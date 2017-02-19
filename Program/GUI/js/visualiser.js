d1eb78149423bf3ff03121107f93559ec21db34f
Leighton Lilford <lrlilford@gmail.com>
2017-02-19 10:13:51 +0800

    Allow edditing of nodes

--- Program/GUI/js/visualiser.js
+++ Program/GUI/js/visualiser.js
@@ -1,3 +1,19 @@
+side_nav_open = false;
+
+function show_side_nav() {
+    if (!side_nav_open) {
+        $('#side_bar').sideNav('show');
+        side_nav_open = true;
+    }
+}
+
+function hide_side_nav() {
+    if (side_nav_open) {
+        $('#side_bar').sideNav('hide');
+        side_nav_open = false;
+    }
+}
+
 function node_color(is_server, is_client) {
     if (is_server) {
         if (is_client) {
@@ -10,25 +26,29 @@ function node_color(is_server, is_client) {
     }
     return "#000";
 }
-$(function() {
+$(function () {
     // SETUP COMPONENTS
     $('#side_bar').sideNav({
         edge: 'right',
         closeOnClick: false,
-        draggable: true
+        draggable: false
     });
 
     // SETUP NETWORK
-    var nodeIds, nodesArray, nodes, edgesArray, edges, network;
-    nodeIds = [1];
-    current_node_id = 1;
+    var nodeIds, nodeDetails, nodes, edgesArray, edges, network;
+    nodeIds = [0];
+    current_node_id = 0;
     current_x = 0;
 
     // create an array with nodes
-    nodesArray = [
-        { id: 1, label: 'Server 1', shape: 'circle', type: 'S', color: node_color(1, 0) },
-    ];
-    nodes = new vis.DataSet(nodesArray);
+    nodeDetails = [{
+        id: 0,
+        label: 'Server 1',
+        shape: 'circle',
+        type: 'S',
+        color: node_color(1, 0)
+    }, ];
+    nodes = new vis.DataSet(nodeDetails);
 
     // create an array with edges
     edgesArray = [];
@@ -51,63 +71,83 @@ $(function() {
         var options = {
             easingFunction: "easeInQuad"
         };
-        network.fit({ animation: options });
+        network.fit({
+            animation: options
+        });
     }
 
     function load_details_bar(node_id) {
         my_node = null;
         type_array = [];
         x = network.nodes;
-        for (i = 0; i < nodesArray.length; i++) {
-            if (nodesArray[i].id == node_id) {
-                my_node = nodesArray[i]
-            }
-        }
+        my_node = nodeDetails[node_id]
         if (my_node !== null) {
             $("#node_name").val(my_node.label);
             $("#node_type").val(my_node.type.split(','));
             $("#node_type").material_select();
-            $('#side_bar').sideNav('show');
+            $("#node_details_btn").text("Save");
+            show_side_nav();
         }
     }
 
-
-    $("#nav_btn_node_new").click(function() {
+    $("#nav_btn_node_new").click(function () {
         $("#node_name").val("");
-        $('#side_bar').sideNav('show');
+        $("#node_details_btn").text("Create");
+        show_side_nav();
     });
-    $("#nav_btn_node_del").click(function() {
+    $("#nav_btn_node_del").click(function () {
         network.deleteSelected();
     })
-    $("#form_node_new").submit(function() {
-        var newId = ++current_node_id;
-        current_x += 100;
+    $("#form_node_new").submit(function () {
+        name = $("#node_name").val();
         s = $("#node_type").val().includes('S');
         c = $("#node_type").val().includes('C');
-        node_to_add = {
-            id: newId,
-            label: $("#node_name").val(),
-            shape: 'circle',
-            color: node_color(s, c),
-            type: s ? c ? 'S,C' : 'S' : 'C',
-            x: current_x
-        };
-        nodesArray.push(node_to_add)
-        nodes.add(node_to_add);
-        nodeIds.push(newId);
+        if ($("#node_details_btn").text() == 'Create') {
+            var newId = ++current_node_id;
+            current_x += 100;
+
+            node_to_add = {
+                id: newId,
+                label: name,
+                shape: 'circle',
+                color: node_color(s, c),
+                type: s ? c ? 'S,C' : 'S' : 'C',
+                x: current_x
+            };
+            nodeDetails.push(node_to_add)
+            nodes.add(node_to_add);
+            nodeIds.push(newId);
+        } else {
+            var selectedNode = network.getSelectedNodes()[0];
+            nodes.update({
+                id: selectedNode,
+                label: name,
+                color: node_color(s, c),
+            });
+            nodeDetails[selectedNode] = {
+                label: name,
+                type: s ? c ? 'S,C' : 'S' : 'C',
+            };
+            network.unselectAll();
+        }
+        hide_side_nav();
+        $("#node_type").val('S');
+        $("#node_type").material_select();
         $("#node_name").val("");
         fitAnimated();
     });
 
-    network.on("click", function(params) {
+    network.on("click", function (params) {
         if (params.nodes.length == 1) {
             load_details_bar(params.nodes[0])
+        } else {
+            hide_side_nav();
         }
     });
 
 
-    $("#btn_close_sideNav").click(function() {
-        $('#side_bar').sideNav('hide');
+    $("#btn_close_sideNav").click(function () {
+        hide_side_nav();
         $("#node_name").val("");
     })
 });
