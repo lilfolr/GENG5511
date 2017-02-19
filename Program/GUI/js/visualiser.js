7219906272b41b62c98b99c59dda869f84b2fe94
Leighton Lilford <lilfolr@live.com.au>
2017-02-19 12:38:23 +0800

    Add edges to nodes

--- Program/GUI/js/visualiser.js
+++ Program/GUI/js/visualiser.js
@@ -26,7 +26,20 @@ function node_color(is_server, is_client) {
     }
     return "#000";
 }
-$(function () {
+
+function alert_msg(msg, level) {
+    if (level == "error") {
+        Materialize.toast(msg, 5000, "red");
+    } else if (level == "warning") {
+        Materialize.toast(msg, 5000, "orange");
+    } else if (level == "info") {
+        Materialize.toast(msg, 5000, "blue");
+    }
+}
+$(function() {
+    //CONSTANTS
+    NODE_SPACING = 200;
+
     // SETUP COMPONENTS
     $('#side_bar').sideNav({
         edge: 'right',
@@ -35,24 +48,29 @@ $(function () {
     });
 
     // SETUP NETWORK
-    var nodeIds, nodeDetails, nodes, edgesArray, edges, network;
-    nodeIds = [0];
-    current_node_id = 0;
-    current_x = 0;
+    var nodeDetails, nodes, edgesArray, edges, network;
+    current_node_id = 1;
+    current_x = NODE_SPACING;
 
-    // create an array with nodes
     nodeDetails = [{
         id: 0,
+        label: 'Client 1',
+        shape: 'circle',
+        type: 'C',
+        color: node_color(0, 1)
+    }, {
+        id: 1,
         label: 'Server 1',
         shape: 'circle',
         type: 'S',
         color: node_color(1, 0)
-    }, ];
+    }];
     nodes = new vis.DataSet(nodeDetails);
 
-    // create an array with edges
-    edgesArray = [];
-    edges = new vis.DataSet(edgesArray);
+    current_edge_id = 0;
+    var edges = new vis.DataSet([
+        { id: 0, from: 0, to: 1 },
+    ]);
 
     // create a network
     var container = document.getElementById('firewall_network');
@@ -63,6 +81,14 @@ $(function () {
     var options = {
         "physics": {
             "enabled": false,
+        },
+        "edges": {
+            "smooth": {
+                "type": "horizontal",
+                "forceDirection": "vertical",
+                "roundness": 0
+            },
+            "color": "black"
         }
     };
     var network = new vis.Network(container, data, options);
@@ -90,22 +116,32 @@ $(function () {
         }
     }
 
-    $("#nav_btn_node_new").click(function () {
+    $("#nav_btn_node_new").click(function() {
         $("#node_name").val("");
         $("#node_details_btn").text("Create");
         show_side_nav();
     });
-    $("#nav_btn_node_del").click(function () {
+    $("#nav_btn_node_del").click(function() {
         network.deleteSelected();
     })
-    $("#form_node_new").submit(function () {
+
+    connect_node_start = null;
+    $("#nav_btn_node_connect").click(function() {
+        if (network.getSelectedNodes().length == 1) {
+            connect_node_start = network.getSelectedNodes()[0];
+            alert_msg('Select node to connect to', 'info');
+        } else {
+            alert_msg('Select a node first', 'warning');
+        }
+    });
+
+    $("#form_node_new").submit(function() {
         name = $("#node_name").val();
         s = $("#node_type").val().includes('S');
         c = $("#node_type").val().includes('C');
         if ($("#node_details_btn").text() == 'Create') {
             var newId = ++current_node_id;
-            current_x += 100;
-
+            current_x += NODE_SPACING;
             node_to_add = {
                 id: newId,
                 label: name,
@@ -116,7 +152,6 @@ $(function () {
             };
             nodeDetails.push(node_to_add)
             nodes.add(node_to_add);
-            nodeIds.push(newId);
         } else {
             var selectedNode = network.getSelectedNodes()[0];
             nodes.update({
@@ -137,16 +172,46 @@ $(function () {
         fitAnimated();
     });
 
-    network.on("click", function (params) {
+    network.on("click", function(params) {
         if (params.nodes.length == 1) {
-            load_details_bar(params.nodes[0])
+            if (connect_node_start !== null) {
+                if (network.getSelectedNodes()[0] == connect_node_start) {
+                    alert_msg("Can't connect node to self", "error");
+                } else {
+                    already_connected = false;
+                    for (i = 0; i < edges.length; i++) {
+                        if (edges._data[i].from == connect_node_start && edges._data[i].to == network.getSelectedNodes()[0]) {
+                            already_connected = true;
+                            break;
+                        } else if (edges._data[i].from == network.getSelectedNodes()[0] && edges._data[i].to == connect_node_start) {
+                            already_connected = true;
+                            break;
+                        }
+                    }
+                    if (already_connected) {
+                        alert_msg("Nodes already connected", "error");
+                    } else {
+                        new_edge = {
+                            id: ++current_edge_id,
+                            from: connect_node_start,
+                            to: network.getSelectedNodes()[0]
+                        }
+                        edges.add(new_edge);
+                        alert_msg("Connected nodes", "info");
+                    }
+                    network.unselectAll();
+                }
+                connect_node_start = null;
+            } else {
+                load_details_bar(params.nodes[0]);
+            }
         } else {
             hide_side_nav();
         }
     });
 
 
-    $("#btn_close_sideNav").click(function () {
+    $("#btn_close_sideNav").click(function() {
         hide_side_nav();
         $("#node_name").val("");
     })
