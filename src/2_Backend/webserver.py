5503f97b7d3f4cd75e98a34776e8f735ecbfce85
Leighton Lilford <lilfolr@live.com.au>
2017-03-18 17:22:53 +0800

    Reformat

--- src/2_Backend/webserver.py
+++ src/2_Backend/webserver.py
@@ -23,34 +23,44 @@ save_results () : json results
 """
 
 from application import *
+import os
 from aiohttp import web
 import socketio
-base_index = '../../1_GUI/'
+
+if os.name == 'nt':
+    base_index = '..\\1_GUI\\'
+else:
+    base_index = '../1_GUI/'
 
 sio = socketio.AsyncServer()
 app = web.Application()
 sio.attach(app)
 
+
 async def index(request):
     """Serve the client-side application."""
-    with open(base_index+'index.html') as f:
+    with open(base_index + 'index.html') as f:
         return web.Response(text=f.read(), content_type='text/html')
+
 
 @sio.on('connect', namespace='')
 def connect(sid, environ):
     print("connect ", sid)
+
 
 @sio.on('chat message', namespace='')
 async def message(sid, data):
     print("message ", data)
     await sio.emit('reply', room=sid)
 
+
 @sio.on('disconnect', namespace='')
 def disconnect(sid):
     print('disconnect ', sid)
 
-app.router.add_static('/css', base_index+'css')
-app.router.add_static('/js', base_index+'js')
+
+app.router.add_static('/css', base_index + 'css')
+app.router.add_static('/js', base_index + 'js')
 app.router.add_get('/', index)
 
 if __name__ == '__main__':
