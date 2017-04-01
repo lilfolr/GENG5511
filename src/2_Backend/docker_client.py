4ead91241cc6047f60906037f582a2e2f750569a
Leighton Lilford <leighton.lilford@bhpilliton.com>
2017-04-01 13:07:02 +0800

    Run iptables restore on boot

--- src/2_Backend/docker_client.py
+++ src/2_Backend/docker_client.py
@@ -16,13 +16,16 @@ class docker_client(object):
         """
         Creates docker container. Returns container id
         """
+        #TODO: check mnt_dir exists
         volume_key = {mnt_dir:{'bind': '/mnt/vol1', 'mode': 'rw'}}
         new_container = self.client.containers.run('test-ip', command='bin/sh', detach=True, tty=True, volumes=volume_key, privileged=True)
+        print("Running install commands")
+        new_container.exec_run("sh -c 'iptables-restore<iptables-save' -s /bin/sh", tty=True, privileged=True,)
         self.containers[new_container.id] = new_container
         return new_container.id
 
     def destroy_container(self, container_id):
-        self.client.containers.get(container_id).kill()
+        self.client.containers.get(container_id).remove(force=True)
 
     def destroy_containers(self):
         for container in self.client.containers.list():
